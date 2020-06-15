d69b28b25be8cdab3353e295e821b370f6bb6765
Bruce Brophy <bruce@brucebrophy.com>
2020-06-14 23:10:43 -0400

    WIP

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,12 +3,34 @@
 namespace Brucebrophy\DadJokes\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Brucebrophy\DadJokes\JokeFactory;
 
 class JokeFactoryTest extends TestCase
 {
 	/** @test */
 	public function it_returns_a_random_joke()
 	{
-		$this->assertTrue(true);
+		$jokes = new JokeFactory([
+			'This is a joke'
+		]);
+		$joke = $jokes->random();
+
+		$this->assertSame('This is a joke', $joke);
+	}
+
+	/** @test */
+	public function it_returns_a_predefined_joke()
+	{
+		$dadJokes = [
+			'6:30 is my favorite time of day, hands down.',
+			'I took up origami for a while, but I gave it up because it was too much paperwork.',
+			'I love my furniture. My recliner and I go way back.',
+		];
+
+		$jokes = new JokeFactory();
+
+		$joke = $jokes->random();
+
+		$this->assertContains($joke, $dadJokes);
 	}
 }
