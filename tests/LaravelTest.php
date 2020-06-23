32b8f2c46df7f67793361a78a3b07fc0d5eadfd6
Bruce Brophy <bruce@brucebrophy.com>
2020-06-22 20:00:04 -0400

    Adds Test for Console Command

---
+++ tests/LaravelTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brucebrophy\DadJokes\Tests;
+
+use Brucebrophy\DadJokes\DadJokeServiceProvider;
+use Brucebrophy\DadJokes\Facades\Joke;
+use Illuminate\Support\Facades\Artisan;
+use Orchestra\Testbench\TestCase;
+
+class LaravelTest extends TestCase
+{
+	protected function getPackageProviders($app)
+	{
+		return [
+			DadJokeServiceProvider::class,
+		];
+	}
+
+	protected function getPackageAliases($app)
+	{
+		return [
+			'Joke' => Joke::class,
+		];
+	}
+
+	public function testConsoleCommandReturnsAJoke()
+	{
+		$this->withoutMockingConsoleOutput();
+
+		$joke = 'Why don’t skeletons ever go trick or treating? Because they have nobody to go with.';
+
+		Joke::shouldReceive('random')
+			->once()
+			->andReturn($joke);
+
+		$this->artisan('joke');
+
+		$output = Artisan::output();
+
+		$this->assertSame($joke.PHP_EOL, $output);
+	}
+}
